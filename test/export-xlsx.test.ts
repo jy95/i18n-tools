cbdbdf4e6236403d5bb83cb23807565c9dbb1845
jy95 <jacques.yakoub@gmail.com>
2021-08-11 20:52:54 +0200

    chore(lint): disable automatic lint

---
+++ test/export-xlsx.test.ts
@@ -0,0 +1,126 @@
+import os from 'os';
+import path from 'path';
+import yargs from 'yargs';
+import {
+  command,
+  description as describeText,
+  builder,
+} from '../src/cmds/export';
+
+// temp folder
+const TEMP_FOLDER = os.tmpdir();
+// test folders constants
+const ROOT_TEST_FOLDER = 'tests-for-export';
+const [VALID_TEST_FOLDER, EMPTY_TEST_FOLDER] = [
+  'correct', // folder where every file are correct
+  'empty', // folder where file has an useless content ([])
+];
+
+// initialise fsify
+const fsify = require('fsify')({
+  cwd: TEMP_FOLDER,
+  persistent: false,
+  force: true,
+});
+
+// Translations keys for test
+const TRANSLATIONS_KEYS = ['FR', 'NL', 'DE'];
+const KEYS_LABEL = {
+  FR: 'French',
+  NL: 'Dutch',
+  DE: 'German',
+};
+const locale_label = (locale: string) => `${KEYS_LABEL[locale]} translation`;
+
+// i18n example
+const generate_i18n = (locale: string) => ({
+  commons: {
+    myNestedKey: `Hello world ${locale}`,
+  },
+  array: ['1', '2', '3'].map(item => `${item} ${locale}`),
+});
+
+// Export columns
+const EXPORT_COLUMNS = (locales: string[]) =>
+  locales.map(locale => ({
+    locale,
+    label: locale_label(locale),
+  }));
+
+// flat operation
+const flat = (arr: any[]) => [].concat(...arr);
+
+// file structure for fsify, in order to run the tests
+const structure = [
+  {
+    type: fsify.DIRECTORY,
+    name: ROOT_TEST_FOLDER,
+    contents: [
+      // In this folder, everything in order
+      {
+        type: fsify.DIRECTORY,
+        name: VALID_TEST_FOLDER,
+        contents: flat([
+          // 3 i18n files
+          TRANSLATIONS_KEYS.map(locale => ({
+            type: fsify.FILE,
+            name: `${locale.toLowerCase()}.json`,
+            contents: JSON.stringify(generate_i18n(locale)),
+          })),
+          // the exportColumns.json
+          {
+            type: fsify.FILE,
+            name: 'exportColumns.json',
+            contents: JSON.stringify(EXPORT_COLUMNS(TRANSLATIONS_KEYS)),
+          },
+          // the files.json
+          {
+            type: fsify,
+            name: 'files.json',
+            contents: JSON.stringify(
+              TRANSLATIONS_KEYS.reduce((acc, locale) => {
+                acc[locale] = path.resolve(
+                  TEMP_FOLDER,
+                  ROOT_TEST_FOLDER,
+                  VALID_TEST_FOLDER,
+                  `${locale.toLowerCase()}.json`
+                );
+                return acc;
+              }, {})
+            ),
+          },
+        ]),
+      },
+    ],
+  },
+];
+
+beforeAll(() => {
+  // useless
+  console.log(EMPTY_TEST_FOLDER);
+  // write temporary files
+  return fsify(structure);
+});
+
+// Build the parser used for that command
+const parser = yargs.command(command, describeText, builder).help();
+
+describe('[export_xlsx command] - tests', () => {
+  it('Should return help output', async () => {
+    // Run the command module with --help as argument
+    const output = await new Promise(resolve => {
+      parser.parse('--help', (_err, _argv, output) => {
+        resolve(output);
+      });
+    });
+
+    // Verify the output is correct
+    expect(output).toBe(expect.stringContaining('xlsx'));
+  });
+
+  /*
+    it("Should parse correctly when conditions are meet", () => {
+        parser.parse()
+    });
+    */
+});
