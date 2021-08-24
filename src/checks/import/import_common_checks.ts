0cff719a7f8c6e57f3c16df9b8d24f19e6132c83
jy95 <jacques.yakoub@gmail.com>
2021-08-24 22:55:31 +0200

    chore(test) : Tests for import-xlsx

--- src/checks/import/import_common_checks.ts
+++ src/checks/import/import_common_checks.ts
@@ -1,17 +1,16 @@
 // lodash methodes
-import uniq from "lodash/uniq";
+import uniq from 'lodash/uniq';
 
 // validation for locales option
 export const LOCALES_CHECK = async (argv: any) => {
+  const locales = argv.locales as any[];
+  if (uniq(locales).length !== locales.length) {
+    return new Error("locales options doesn't contain uniq values");
+  }
 
-    const locales = argv.locales as any[];
-    if (uniq(locales).length !== locales.length) {
-        return new Error("locales options doesn't contain uniq values");
-    }
-
-    // pass validation
-    return true;
-}
+  // pass validation
+  return true;
+};
 
 // export checks in expected order into a single array
 export const CHECKS = [LOCALES_CHECK];
